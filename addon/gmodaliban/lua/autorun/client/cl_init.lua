5492695794fd4f0b3e4e9d979e82c1b532566439
DESKTOP-QBKIBUM\a0103 <a0103932@naver.com>
2017-06-11 02:34:38 +0900

    no message

---
+++ addon/gmodaliban/lua/autorun/client/cl_init.lua
@@ -0,0 +1,41 @@
+﻿if CLIENT then
+
+net.Receive( "AliBanMenu", function( len, ply )
+if(LocalPlayer():IsSuperAdmin()) then
+local Frame = vgui.Create( "DFrame" )
+Frame:SetSize( 300, 200 )
+Frame:SetTitle( "연합벤 시스템" )
+Frame:SetVisible( true )
+Frame:SetDraggable( true )
+Frame:ShowCloseButton( true )
+Frame:MakePopup()
+	
+local DermaButton = vgui.Create( "DButton", Frame )
+DermaButton:SetText( "연합벤 목록 업데이트" )
+DermaButton:SetPos( 25, 50 )
+DermaButton:SetSize( 250, 30 )
+DermaButton.DoClick = function()	
+	RunConsoleCommand("aliban_update")
+end
+
+local DermaButton = vgui.Create( "DButton", Frame )
+DermaButton:SetText( "연합벤 홈페이지" )
+DermaButton:SetPos( 25, 100 )
+DermaButton:SetSize( 250, 30 )
+DermaButton.DoClick = function()	
+	gui.OpenURL( "https://lill74.github.io/Garrys-mod-Ali-Bans/" )
+end
+
+local TextEntry = vgui.Create( "DTextEntry", Frame ) -- create the form as a child of frame
+TextEntry:SetPos( 25, 150 )
+TextEntry:SetSize( 250, 30 )
+TextEntry:SetText( net.ReadString())
+TextEntry.OnEnter = function( self )
+	chat.AddText( self:GetValue() )	-- print the form's text as server text
+end
+
+else
+return false
+end
+end)
+end
---
+++ addon/gmodaliban/lua/autorun/server/sv_init.lua
@@ -0,0 +1,90 @@
+﻿AliBansConfig = {}
+local Config = AliBansConfig
+Config.Version = "1.0" --연합벤 현재 버전
+Config.Commander = "!연합벤" --연합벤 메뉴 명령어
+
+
+if SERVER then
+util.AddNetworkString( "AliBanMenu" )
+    print("[AliBans] AliBan System Loaded")
+	banlist = {}
+	content = ""
+	local url = "https://raw.githubusercontent.com/lill74/Garrys-mod-Ali-Bans/master/ban.html"
+	
+	function check()
+	http.Fetch(url, function(html)
+			banlist = string.Explode("|",html)
+			content = html
+			if(banlist[1] > Config.Version) then
+			print("[AliBans] Please update aliban addon!")
+
+			for k, v in pairs(player.GetAll()) do
+				v:PrintMessage( HUD_PRINTTALK, "[AliBans] 새로운 업데이트가 있습니다!" )
+			end
+
+			end
+			
+			for k, v in pairs(player.GetAll()) do
+				v:PrintMessage( HUD_PRINTTALK, "[AliBans] 데이터베이스 업데이트 완료!" )
+			end
+		    print("Complete updating aliban list")
+	end)
+	end
+	
+	function usercheck()
+			for k, v in pairs(player.GetAll()) do
+				for _,ban in pairs(banlist) do
+					if ban == v:SteamID() then
+					    kickuser(v)
+					end
+				end
+			end
+	end
+	
+	function aliget(v)
+	usercheck()
+				for _,ban in pairs(banlist) do
+					if ban == v:SteamID() then
+					     kickuser(v)
+					end
+				end
+	end
+	
+             function kickuser(v)
+             	 print("Kick " .. v:Nick() .. " by AliBans")
+             	user:Kick("[AliBans] 연합벤 사유: https://goo.gl/pU8aCC")
+             	for k, v in pairs(player.GetAll()) do
+				v:PrintMessage( HUD_PRINTTALK, user:Nick() .. " 님이 연합벤 시스템에 의해 차단당했습니다!")
+		end
+             end
+
+	function init()
+	check()
+	timer.Create("UpdateAlibanList", 120 , 0, check)
+	print("[AliBans] Initialize AliBan System")
+	end
+	
+	hook.Add( "PlayerInitialSpawn", "CheckUsers", aliget )
+	hook.Add( "Initialize", "InitializeAliBans", init )
+	
+	concommand.Add( "aliban_update", function( ply, cmd, args )
+	if(ply:IsSuperAdmin()) then
+	check()
+	else
+	return false
+	end
+end )
+
+
+	hook.Add( "PlayerSay", "AliBanMenu", function( ply, text, public )
+	if ( text == Config.Commander ) then
+	if(ply:IsSuperAdmin()) then
+	net.Start( "AliBanMenu" )
+	net.WriteString(content)
+    net.Send(ply)
+	else
+	return false
+	end
+	end
+	end)
+end
